977c1922882b7cd070badbfc2832d06a91784c30
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-04-17 23:06:33 -0600

    Add files via upload

---
+++ NetBeansProjects/jfrmCal/src/jfrmcal/JfrmCal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jfrmcal;
+import Formulario.jfrmC;
+/**
+ *
+ * @author Administrator
+ */
+public class JfrmCal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        jfrmC f = new jfrmC();
+        f.setVisible(true);
+        
+    }
+    
+}
